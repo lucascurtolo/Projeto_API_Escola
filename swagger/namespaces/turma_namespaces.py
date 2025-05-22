c0e5a7b8c6f4cf5c396cd92c74c79462f9495310
Marcelo Augusto Ribeiro Martins <marcelo0204martins@gmail.com>
2025-05-22 15:27:10 -0300

    Id disciplina no swagger

--- swagger/namespaces/turma_namespaces.py
+++ swagger/namespaces/turma_namespaces.py
@@ -14,7 +14,8 @@ turmas_model = turmas_ns.model("Turma", {
 turmas_output_model = turmas_ns.model("TurmasOutput", {
     "id": fields.Integer(description="Id da turma"),
     "nome": fields.String(description="Nome da turma"),
-    "professor_id": fields.Integer(description= "Id do professor da turma")
+    "professor_id": fields.Integer(description= "Id do professor da turma"),
+    "id_disciplina": fields.Integer(description= "Id da disciplina")
 })
 
 
@@ -34,7 +35,7 @@ class TurmaResource(Resource):
         nome = data['nome']
         professor_id = data['professor_id']
 
-        turma = turmas_repo.criar_turma(id=None ,nome=nome, professor_id=professor_id)
+        turma = turmas_repo.criar_turma(id=None ,id_disicplina=None,nome=nome, professor_id=professor_id)
 
         return turma.to_dict(), 201
 
@@ -49,11 +50,11 @@ class TurmaIdResource(Resource):
             return {"message": "Turma não encontrada"}, 404
     
     @turmas_ns.expect(turmas_model) 
-    def put(self, id):
+    def put(self, id, id_disciplina):
         data = turmas_ns.payload
         nome = data["nome"]
         professor_id = data["professor_id"]
-        turma =  turmas_repo.atualizar_turma(id=id, nome=nome, professor_id=professor_id)
+        turma =  turmas_repo.atualizar_turma(id=id, nome=nome, professor_id=professor_id, id_disciplina=id_disciplina)
         return turma.to_dict(), 200
     
     def delete(self,id):
